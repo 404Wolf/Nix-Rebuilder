6e5f15ab72e35706826a9d3cdb015ecc1d8c420f
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-06-10 13:41:40 -0400

    Update for home manager

--- src/nix-rebuilder.bash
+++ src/nix-rebuilder.bash
@@ -5,6 +5,10 @@ configDir=$(cat ~/.config/nix-builder-script/config.json | jq .configDir -r)
 echo "Config dir: $configDir";
 outputDir="/etc/nixos";
 
+# Copy over neovim config
+cp -r ~/.config/nvim $configDir/src/config/nvim;
+tar -czvf ~/.config/nvim.tar.gz ~/.config/nvim; # Backup the neovim config
+
 # Make a unified commit message for the configuration
 git -C $configDir pull;
 version=$(git -C $configDir log --oneline | wc -l);
@@ -27,5 +31,21 @@ echo "Commited and pushed all changes to nix config dir";
 sudo git -C $outputDir pull;
 echo "Pulled latest changes from the nix config dir";
 
-# Rebuild the system
-sudo nixos-rebuild --flake /etc/nixos switch;
+# Figure out if the user command is 'nix' or 'home'
+COMMAND=$1;
+
+# If the command is 'nix', then rebuild the system
+case $COMMAND in
+    "nix")
+        sudo nixos-rebuild --flake $outputDir switch;
+        exit 0;
+        ;;
+    "home")
+        sudo home-manager --flake $outputDir#wolf switch;
+        exit 0;
+        ;;
+    *)
+        echo "Invalid command: $COMMAND";
+        exit 1;
+        ;;
+esac
